0ceff56bb52be14ba1532b2eb02ddebc26b940bc
julianamxg <jamelo827@misena.edu.co>
2022-09-23 23:51:27 -0500

    primerCommit
    Nav

---
+++ src/components/nav/nav.js
@@ -0,0 +1,20 @@
+import logov from "./Hotelia horizontal blanco.svg";
+import "./nav.css";
+function Nav(){
+    return(
+        <nav>
+        <div class="logo">
+                <i class="fa-solid fa-bars icono"></i>
+                <img src={logov} alt="Logo Hotelia"/>
+        </div>
+        <div class="menu">
+                <a href="index.html" class="item">Inicio</a>
+                <a href="ubicacion.html" class="item">Ubícanos</a>
+                <a href="opiniones.html" class="item">Opiniones</a>
+                <a href="assets/login.html" class="item2"><i class="fa-solid fa-user"></i> Iniciar Sesión</a>
+        </div>
+    </nav>
+    );
+}
+
+export default Nav;
